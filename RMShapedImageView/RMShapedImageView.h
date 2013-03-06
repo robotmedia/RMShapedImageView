85f7915906d138c475fbb1c5e9724a5a4102c0e6
Hermes Pique <hermes.pique@robotmedia.net>
2013-03-06 15:27:15 +0100

    Add class description

--- RMShapedImageView/RMShapedImageView.h
+++ RMShapedImageView/RMShapedImageView.h
@@ -23,6 +23,10 @@
 
 #import <UIKit/UIKit.h>
 
+/** A UIImageView subclass that ignores touches on transparent pixels, based on OBShapedButton by Ole Begemann.
+ 
+ RMShapedImageView does it magic by overriding pointInside:withEvent:. This method is called to determine if a touch is inside the view. In our case, we only want to return YES if the corresponding pixels are not transparent.
+ */
 @interface RMShapedImageView : UIImageView
 
 /** Number of pixels around the point that will be examined. If at least one of them has alpha bigger than shapedTransparentMaxAlpha pointInside:withEvent: will return YES. 0 by default.
